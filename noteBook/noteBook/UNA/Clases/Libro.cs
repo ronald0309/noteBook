796ec9cc46245d0a23ce1947beec192568018bfa
GerardoAc <gera.araya3@gmail.com>
2019-12-15 16:55:14 -0600

    Creacion de la clase Libro

---
+++ noteBook/noteBook/UNA/Clases/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace noteBook.UNA.Clases
+{
+    class Libro
+    {
+        public Libro() { 
+        
+        }
+
+        public List<ListBox> Notas = new List<ListBox>();
+        public List<ListBox> Lista
+        {
+            get { return Notas; }
+        }
+        public void NotaNueva(ListBox nota) {
+            Lista.Add(nota);
+
+        }
+        public int pocision {
+            get;
+            set;
+        }
+
+    }
+}
