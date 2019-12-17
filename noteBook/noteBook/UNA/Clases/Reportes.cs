0a5eaba6f0b2791fb805535b89286822f91ec89f
ronald0309 <ronaldba16@gmail.com>
2019-12-16 22:17:50 -0600

    se crea la clase Usuario y la clase Reportes

---
+++ noteBook/noteBook/UNA/Clases/Reportes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace noteBook.UNA.Clases
+{
+    public class Reportes
+    {
+        public DateTime FechaCreacion
+        {
+            get;
+            set;
+        }
+        public String UsuarioActual
+        {
+            get;
+            set;
+        }
+        public string AccionRealizada
+        {
+            get;
+            set;
+        }
+        public string InformacionAdicional
+        {
+            get;
+            set;
+        }
+    }
+    
+}
---
+++ noteBook/noteBook/UNA/Clases/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace noteBook.UNA.Clases
+{
+    public class Usuario
+    {
+        public Usuario()
+        {
+
+        }
+        public string NombreUsuario
+        {
+            get;
+            set;
+        }
+        public string Contarseña
+        {
+            get;
+            set;
+        }
+       
+    }
+}
