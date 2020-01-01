06485536bda3642f043e73f2cada97711a7a631f
ronald0309 <ronaldba16@gmail.com>
2020-01-01 10:12:16 -0600

    Se modifica la clase editar notas

--- noteBook/noteBook/UNA/vistas/EditarNotasForm.cs
+++ noteBook/noteBook/UNA/vistas/EditarNotasForm.cs
@@ -1,99 +1,345 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.ComponentModel;
+
 using System.Data;
+
 using System.Drawing;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
+
 using noteBook.UNA.Clases;
+
 namespace noteBook.UNA.vistas
+
 {
+
     public partial class EditarNotasForm : Form
+
     {
+
         private Nota not;
+
+        private bool guardoDatos = false;
+
         public EditarNotasForm()
+
         {
+
             InitializeComponent();
+
+            mensajes();
+
         }
+
+        private void mensajes()
+
+        {
+
+            mensajeEditarNotas.SetToolTip(lblTitulo, "Titulo actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(TXTTitulo, "Ingrese el nuevo titulo");
+
+            mensajeEditarNotas.SetToolTip(SelectorColoresNotas, "Color de fondo actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(PBColorFondo, "Ingrese el nuevo color de fondo de la nota");
+
+            mensajeEditarNotas.SetToolTip(colorFuente, "Color de fuente actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(PBColorFuente, "Ingrese el nuevo color de fondo");
+
+            mensajeEditarNotas.SetToolTip(lblFuente, "Estilo de la fuente actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(CBXFuente, "Ingrese el nuevo estilo de fuente");
+
+            mensajeEditarNotas.SetToolTip(lblPrivacidad, "Privacidad actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(CBXPrivacidad, "Ingrese la nueva privacidad de la nota");
+
+            mensajeEditarNotas.SetToolTip(lblCategoria, "Categoria actual de la nota");
+
+            mensajeEditarNotas.SetToolTip(TXTCategoria, "Ingrese la nueva categoria de la nota");
+
+            mensajeEditarNotas.SetToolTip(btnAceptar, "Guardar modificacion");
+
+            mensajeEditarNotas.SetToolTip(btnCancelar, "Cancelar modificación");
+
+        }
+
         public void CargarDatos(Object nota)
+
         {
+
             not = (Nota)nota;
+
             lblTitulo.Text = not.Titulo;
-            lblPrivacidad.Text =Convert.ToString( not.Privacidad);
+
             lblFuente.Text = not.Fuente;
-            colorDialog1.Color= Color.FromArgb(not.ColorFondo);
+
+            colorDialog1.Color = Color.FromArgb(not.ColorFondo);
+
             colorDialog2.Color = Color.FromArgb(not.ColorFuente);
-            SelectorColoresNotas.BackColor = Color.FromArgb (not.ColorFondo);
+
+            SelectorColoresNotas.BackColor = Color.FromArgb(not.ColorFondo);
+
             colorFuente.BackColor = Color.FromArgb(not.ColorFuente);
+
             PBColorFondo.BackColor = Color.FromArgb(not.ColorFondo);
+
             PBColorFuente.BackColor = Color.FromArgb(not.ColorFuente);
-            foreach (FontFamily font in FontFamily.Families)
+
+            if (not.Privacidad)
+
             {
-                CBXFuente.Items.Add(font.Name.ToString());
- 
+
+                lblPrivacidad.Text = "Privado";
+
             }
-        }
-        public void ModificarNota()
-        {
-           
-            foreach (var libro in Singlenton.Instance.LibrosList)
+
+            else
+
             {
+
+                lblPrivacidad.Text = "Publico";
+
+
+
+            }
+
+            foreach (FontFamily font in FontFamily.Families)
+
+            {
+
+                CBXFuente.Items.Add(font.Name.ToString());
+
+
+
+            }
+
+        }
+
+        public void ModificarNota()
+
+        {
+
+
+
+            foreach (var libro in Singlenton.Instance.LibrosList)
+
+            {
+
                 foreach (var nota in libro.AgregarNota)
+
                 {
-                    if(not.Titulo==nota.Titulo)
+
+                    if (not.Titulo == nota.Titulo)
+
                     {
-                        if(TXTTitulo.TextLength!=0)
+
+                        if (TXTTitulo.TextLength != 0)
+
                         {
+
                             nota.Titulo = TXTTitulo.Text;
+
                         }
-                        if (CBXPrivacidad.SelectedIndex >=0)
+
+                        if (TXTCategoria.TextLength != 0)
+
                         {
-                          //  nota.Privacidad = CBXPrivacidad.SelectedIndex;
+
+                            nota.Categoria = TXTCategoria.Text;
+
                         }
+
+                        if (CBXPrivacidad.SelectedIndex >= 0)
+
+                        {
+
+
+
+                            if (CBXPrivacidad.Text == "Publico")
+
+                            {
+
+                                nota.Privacidad = false;
+
+                            }
+
+                            else
+
+                            {
+
+                                if (CBXPrivacidad.Text == "Privado")
+
+                                {
+
+                                    nota.Privacidad = true;
+
+
+
+                                }
+
+                            }
+
+
+
+                        }
+
                         if (CBXFuente.SelectedIndex >= 0)
+
                         {
+
                             nota.Fuente = CBXFuente.Text;
+
                         }
+
                         nota.ColorFuente = colorDialog2.Color.ToArgb();
+
                         nota.ColorFondo = colorDialog1.Color.ToArgb();
 
+                        DateTime hoy = DateTime.Now;
+
+                        nota.FechaModificacion = Convert.ToString(hoy);
+
                     }
+
                 }
+
             }
+
         }
+
+
 
         private void PBColorFondo_Click(object sender, EventArgs e)
+
         {
+
             if (colorDialog1.ShowDialog() == DialogResult.OK)
+
             {
+
+
 
                 PBColorFondo.BackColor = colorDialog1.Color;
+
             }
+
         }
+
+
 
         private void PBColorFuente_Click(object sender, EventArgs e)
+
         {
-            if (colorDialog1.ShowDialog() == DialogResult.OK)
+
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+
             {
 
-                PBColorFuente.BackColor = colorDialog1.Color;
+
+
+                PBColorFuente.BackColor = colorDialog2.Color;
+
             }
+
         }
+
+
 
         private void btnCancelar_Click(object sender, EventArgs e)
+
         {
-            this.Close();
+
+            if (!guardoDatos)
+
+            {
+
+                //essageBox.Show("Desea salir sin guardar el proyecto", "alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                MessageBoxButtons botones = MessageBoxButtons.YesNoCancel;
+
+                DialogResult dr = MessageBox.Show("Desea salir sin guardar", "Alerta", botones, MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+
+                {
+
+                    this.Close();
+
+                }
+
+                else
+
+                {
+
+                    if (dr == DialogResult.No)
+
+                    {
+
+                        ModificarNota();
+
+                        MessageBox.Show("Se modifico la nota");
+
+                        this.Close();
+
+                    }
+
+
+
+
+
+                }
+
+
+
+            }
+
         }
+
+
+
+
 
         private void btnAceptar_Click(object sender, EventArgs e)
+
         {
-            ModificarNota();
-            MessageBox.Show("Se modifico la nota");
-            this.Close();
+
+            try
+
+            {
+
+                ModificarNota();
+
+                MessageBox.Show("Se modifico la nota");
+
+                this.Close();
+
+            }
+
+            catch (Exception Ex)
+
+            {
+
+                MessageBox.Show("Se produjo un error al guardar la nota");
+
+            }
+
+
 
         }
+
     }
-    
+
+
+
 }
