434c00b928a4bfbc47424a12225cb996a77c4256
GerardoAc <gera.araya3@gmail.com>
2020-01-19 16:15:07 +0100

    Implementacion de las clases para administrar la base de datos

---
+++ noteBook/noteBook/DbAccess.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace noteBook
+{
+    public abstract class DbAccess
+    {
+        public String ConnectionString
+        {
+            get;
+            set;
+        }
+        public MySqlTransaction DbTransaction
+        {
+            get;
+            set;
+        }
+        public MySqlConnection DbConnection
+        {
+            get;
+            set;
+        }
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+        public abstract DataTable QuerySQL(String query);
+        public abstract long EjectSQL(String query);
+        public abstract Boolean IsTransaction();
+        public abstract void CommitTransaction();
+        public abstract void RollbackTransaction();
+        public abstract void BeginTransaction();
+    }
+}
